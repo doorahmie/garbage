c5c66b1fd1082eac660d5085c57e1520f95678bb
doorahmie <doorahmie@doorahmies-Mac-mini.local>
2023-12-07 15:07:22 +0900

    drag picker

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # landfill
+
+아무거나 예시를 직접 실행해본 결과 기록 
+
+[datepicker](datepicker/)
---
+++ datepicker/index.js
@@ -0,0 +1,89 @@
+console.clear();
+
+// idle
+// -> onPointerDown
+// dragging
+// -> onPointerMove / onPointerOver
+// <- onPointerUp
+
+/* NOTE: We retooled the data to be based on first selection and second selection. We'll calculate which is the start & end date later in the `updateDOM` function. */
+const data = {
+  firstDate: null,
+  secondDate: null
+};
+
+const machine = {
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        pointerdown: (data, event) => {
+          data.firstDate = +event.currentTarget.dataset.day;
+          data.secondDate = null;
+          return 'dragging';
+        }
+      }
+    },
+    dragging: {
+      on: {
+        pointerover: (data, event) => {
+          data.secondDate = +event.currentTarget.dataset.day;
+          
+          return 'dragging';
+        },
+        pointerup: 'idle',
+        pointercancel: 'idle'
+      }
+    }
+  }
+};
+
+// idle
+let currentState = machine.initial;
+
+function send(event) {
+  const transition = machine
+    .states[currentState]
+    .on[event.type];
+
+  if (typeof transition === 'function') {
+    currentState = transition(data, event);
+    updateDOM();
+  } else if (transition) {
+    currentState = transition;
+    updateDOM();
+  }
+}
+
+/* ---------------------------------- */
+
+const allDayEls = document.querySelectorAll('[data-day]');
+
+allDayEls.forEach(dayEl => {
+  dayEl.addEventListener('pointerdown', send);
+  dayEl.addEventListener('pointerover', send);
+});
+
+document.body.addEventListener('pointerup', send);
+
+/* ---------------------------------- */
+
+function updateDOM(){
+  document.querySelectorAll('[data-selected]')
+    .forEach(el => {
+      delete el.dataset.selected
+    });
+  
+  const startDate = Math.min(data.firstDate, data.secondDate);
+  const endDate = Math.max(data.firstDate, data.secondDate);
+  
+  if ( startDate ) {
+    const startDateEl = document.querySelector(`[data-day="${startDate}"]`);
+    startDateEl.dataset.selected = "start";
+  }
+  
+  if ( endDate ) {
+    const endDateEl = document.querySelector(`[data-day="${endDate}"]`);
+    endDateEl.dataset.selected = "end";
+  }
+}
